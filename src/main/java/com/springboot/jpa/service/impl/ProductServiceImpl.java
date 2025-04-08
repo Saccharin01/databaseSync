ea205d409ce9aa16661bebe773416817cdba66ef
saccharin01 <chousik01@gmail.com>
2025-04-08 13:36:17 +0900

    feat | ProductServiceImpl.java
    ProductService 인터페이스를 구현하는 구현체의 스켈레톤 코드 제거 및 로직 추가

--- src/main/java/com/springboot/jpa/service/impl/ProductServiceImpl.java
+++ src/main/java/com/springboot/jpa/service/impl/ProductServiceImpl.java
@@ -2,10 +2,14 @@ package com.springboot.jpa.service.impl;
 
 import com.springboot.jpa.data.dao.ProductDAO;
 import com.springboot.jpa.data.dto.ProductDTO;
+import com.springboot.jpa.data.entity.Product;
+
 import com.springboot.jpa.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import com.springboot.jpa.data.dto.ProductResponseDTO;
+
+import java.time.LocalDateTime;
 
 @Service
 public class ProductServiceImpl implements ProductService {
@@ -14,26 +18,60 @@ public class ProductServiceImpl implements ProductService {
 
     @Autowired
     public ProductServiceImpl(ProductDAO productDAO) {
+
         this.productDAO = productDAO;
     }
 
     @Override
-    public ProductResponseDTO getProduct(Long number){
-        return null;
+    public ProductResponseDTO getProduct(Long number) {
+
+
+        //todo Builder 패턴으로 변경도 고려 해봐야 함
+        //todo ProductDAO의 selectProduct 인터페이스는 추후 예외 객체를 반환하도록 변경될 수 있음
+
+        Product product = productDAO.selectProduct(number);
+
+        ProductResponseDTO response = new ProductResponseDTO();
+
+        response.setNumber(product.getNumber());
+        response.setName(product.getName());
+        response.setPrice(product.getPrice());
+        response.setStock(product.getStock());
+
+        return response;
     }
 
     @Override
     public ProductResponseDTO saveProduct(ProductDTO productDTO) {
+        Product product = new Product();
+        product.setName(productDTO.getName());
+        product.setPrice(productDTO.getPrice());
+        product.setStock(productDTO.getStock());
+        product.setCreateAt(LocalDateTime.now());
+        product.setUpdateAt(LocalDateTime.now());
+
+        Product saveProduct = productDAO.insertProduct(product);
         return null;
     }
 
     @Override
-    public ProductResponseDTO changeProductName(Long number, String productName) throws Exception {
-        return null;
+    public ProductResponseDTO changeProductName(Long number, String name) throws Exception {
+
+        Product changedItems = productDAO.updateProductName(number, name);
+
+
+        ProductResponseDTO response = new ProductResponseDTO();
+        response.setNumber(changedItems.getNumber());
+        response.setName(changedItems.getName());
+        response.setPrice(changedItems.getPrice());
+        response.setStock(changedItems.getStock());
+
+        return response;
     }
 
     @Override
-    public void deleteProduct(Long number) {
+    public void deleteProduct(Long number) throws Exception {
+        productDAO.deleteProduct(number);
 
     }
 }
