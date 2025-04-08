1fd3153f5d5b35fdd026c08dea6a1fd70292751e
saccharin01 <chousik01@gmail.com>
2025-04-08 11:17:41 +0900

    feat | ProductDAOImpl.java
    인터페이스 상속 구현체의 스켈레톤 부분을 실제 메서드로 정의

--- src/main/java/com/springboot/jpa/data/dao/impl/ProductDAOImpl.java
+++ src/main/java/com/springboot/jpa/data/dao/impl/ProductDAOImpl.java
@@ -6,6 +6,9 @@ import com.springboot.jpa.data.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import java.time.LocalDateTime;
+import java.util.Optional;
+
 @Component
 public class ProductDAOImpl implements ProductDAO {
 
@@ -18,19 +21,47 @@ public class ProductDAOImpl implements ProductDAO {
 
     @Override
     public Product insertProduct(Product product) {
-        return null;
+        Product insertedProduct = productRepository.save(product);
+        return insertedProduct;
     }
 
     @Override
-    public Product selectProduct(Long number) {
-        return null;
+    public Product selectProduct(Long number) throws Exception {
+        Product selected = productRepository.findById(number)
+        .orElseThrow(()-> new Exception("요청한 내용을 찾을 수 없습니다."));
+        return selected;
     }
 
     @Override
     public Product updateProductName(Long number, String name) throws Exception {
-        return null;
+        Optional<Product> selected = productRepository.findById(number);
+
+        Product updatedProduct;
+        if (selected.isPresent()) {
+
+            Product updateItem = selected.get();
+
+            updateItem.setName(name);
+            updateItem.setUpdateAt(LocalDateTime.now());
+
+            updatedProduct = productRepository.save(updateItem);
+        } else {
+            throw new Exception("선택된 항목을 변경할 수 없습니다.");
+        }
+
+        return updatedProduct;
     }
 
     @Override
-    public void deleteProduct(Long number) throws Exception {}
+    public void deleteProduct(Long number) throws Exception {
+        Optional<Product> selected = productRepository.findById(number);
+        if (selected.isPresent()) {
+            Product deleteItem = selected.get();
+
+            productRepository.delete(deleteItem);
+        } else {
+            throw new Exception("선택된 항목을 삭제할 수 없습니다.");
+        }
+
+    }
 }
